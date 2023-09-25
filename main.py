f9c205e8b4fc928195b499bbcfc24d388c19aa21
Замрій Олег Володимирович <143324688+OlegZamriy@users.noreply.github.com>
2023-09-25 22:04:45 +0300

    11

---
+++ main.py
@@ -0,0 +1,12 @@
+num1 = float(input("Введіть перше число: "))
+num2 = float(input("Введіть друге число: "))
+
+suma = num1 + num2
+
+riznycya = num1 - num2
+
+dobutok = num1 * num2
+
+print(f"Сума чисел: {suma}")
+print(f"Різниця чисел: {riznycya}")
+print(f"Добуток чисел: {dobutok}")
